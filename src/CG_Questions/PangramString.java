7098f838d165f99b113672048cb9cf7e00b21b97
arshsaini15 <arshdeepp015@gmail.com>
2026-01-09 10:26:58 +0530

    Pangram String

--- src/CG_Questions/PangramString.java
+++ src/CG_Questions/PangramString.java
@@ -1,26 +1,30 @@
 package CG_Questions;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class PangramString {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String str = input.nextLine();
 
-        if (str == null || str.length() < 26) {
-            System.out.println("Not Pangram");
-        }
+        boolean flag = true;
+        String s = "abcdefghijklmnopqrstuvwxyz"; // a - z
 
-        Set<Character> letters = new HashSet<>();
-
-        for (char c : str.toLowerCase().toCharArray()) {
-            if (c >= 'a' && c <= 'z') {
-                letters.add(c);
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!str.contains(String.valueOf(c))) {
+                flag = false;
+                break;
             }
         }
 
-        System.out.println("Is Pangram : " + (letters.size() == 26));
+        if(flag) {
+            System.out.println("Pangram");
+        }
+        else {
+            System.out.println("Not Pangram");
+        }
+
+
     }
 }
